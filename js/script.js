0c53f85c856ee44ac55c90db4094d1f1878644fe
Victor-Estrella <victorhcarracci@gmail.com>
2024-08-05 11:25:17 -0300

    Lendo array com loop FOR tradicional.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Declarando um array
+let frutas = ["Zimbro", "Kiwi", "Yamamomo", "Banana", "Wampe"];
+//Visualizando o conteúdo do Array
+console.log(frutas);
+console.log(frutas[4]);
+console.table(frutas);
+
+//Realizando a leitura de um array com o FOR TRADICIONAL
+for (let x = 0; x < frutas.length; x++) {
+    console.log(frutas[x]);
+}
