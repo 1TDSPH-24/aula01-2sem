873f9ad76cee4f1f7e0982b0cee98133a72d7915
FernandoHirashima <fernando.hitoshi@hotmail.com.br>
2024-08-05 11:33:03 -0300

    For OF e  for IN

--- js/script.js
+++ js/script.js
@@ -3,9 +3,24 @@ let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampee"];
 //visualizando o conteudo do ARRAY:
 console.log(frutas);
 console.log(frutas[4]);
-console.table(frutas);
+console.table(frutas); 
 //Realizando a leitura de um array com FOR TRADICONAL 
 for (let x = 0; x < frutas.length; x++) {
     console.log(frutas[x]);
+}
 
+console.log("=============================================");
+
+
+//Realizando a leitura de um array com o FOR OF
+for (let fruta of frutas) {
+    console.log(fruta[indice]);
+}
+
+console.log("=============================================");
+
+
+//Realizando a leitura de um array com o FOR IN
+for (let indice in frutas) {
+    console.log(fruta[indice]);
 }
