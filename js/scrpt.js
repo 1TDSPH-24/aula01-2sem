fae4760c3eea218e096d42464768ddd118c3a8e1
lecristina <cristinale33@hotmail.com>
2024-08-05 11:51:55 -0300

    PUSH, UNSHIFT, POP, SHIFT, SORT e REVERSE

--- js/scrpt.js
+++ js/scrpt.js
@@ -6,7 +6,7 @@ console.log(frutas);
 console.log(frutas[4]);
 console.table(frutas);
 
-//Realizando a leitura de um array com o FOR tradicional
+//Realizando a leitura de um array com o FOR tradicional (bom para comparar)
 for (let x = 0; x < frutas.length; x++) {
     console.log(frutas[x]);
 
@@ -15,7 +15,7 @@ for (let x = 0; x < frutas.length; x++) {
 console.log("================================================");
 
 
-//Realizando a leitura de um array com o FOR OF (+para leitura)
+//Realizando a leitura de um array com o FOR OF (+para leitura) (bom para comparar)
 for (let fruta of frutas) {
     console.log(fruta);
 }
@@ -23,9 +23,50 @@ for (let fruta of frutas) {
 console.log("================================================");
 
 
-//Realizando a leitura de um array com o FOR IN (+para retornar índice)
+//Realizando a leitura de um array com o FOR IN (+para retornar índice) (bom para comparar)
 for (let indice in frutas) {
     console.log(frutas[indice]);
 }
 
+console.log("================================================");
 
+
+//Realizando a leitura de um array com o FOR EACH (NÃO É BOM PARA COMPARAR)
+frutas.forEach(function (fruta, indice, futasArray) {
+    console.log("COM FOR-EACH", fruta, indice, futasArray[indice]);
+});
+
+console.log("================================================");
+
+//Inserindo novos dados com PUSH e UNSHIFT
+frutas.unshift("melancia");
+frutas.shift("melão");;
+for (let fruta of frutas) {
+    console.log(fruta);
+
+}
+
+console.log("================================================");
+
+//Inserindo novos dados com POP(FIM) e SHIFT(INÍCIO)
+frutas.shift();
+frutas.pop();
+for (let fruta of frutas) {
+    console.log(fruta);
+
+}
+
+console.log("================================================");
+
+//Alterando a ordem dos dados com SORT(ordem alfabética) e REVERSE(alterando a ordem atual).
+frutas.sort();
+for (let fruta of frutas) {
+    console.log(fruta);
+
+}
+console.log("================================================");
+frutas.reverse();
+for (let fruta of frutas) {
+    console.log(fruta);
+
+}
