72b89b3250e6e9bcbf7e85745176cbc15fed5565
p3terhenry <pedro.antonieti@outlook.com>
2024-08-05 11:46:42 -0300

    ForEach.

--- js/script.js
+++ js/script.js
@@ -9,16 +9,23 @@ for(let x = 0; x < frutas.length; x++){
     console.log(frutas[x]);
 }
 
-console.log("==========================================================");
+console.log("=================================================================================");
 
 //Realizando a leitura de um array com o FOR OF. Pega uma item do frutas e coloca em fruta.
 for (let fruta of frutas){
     console.log(fruta);
 }
 
-console.log("==========================================================");
+console.log("=================================================================================");
 
 //Realizando a leitura de um array com FOR IN. Pega o valor do indice da fruta de frutas.
 for (let fruta in frutas) {
     console.log(frutas[fruta]);
 }
+
+console.log("=================================================================================");
+
+frutas.forEach(function(fruta, indice, lista) {
+    console.log(fruta, indice, lista);
+});
+
