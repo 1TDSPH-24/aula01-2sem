d44a703e750b99144c967080dc68013455ca8095
giopolub <giovanni.polub.orlandi@gmail.com>
2024-08-05 12:11:12 -0300

    Continuando manipulação com indexOf e Splice

--- js/script.js
+++ js/script.js
@@ -58,3 +58,21 @@ frutas.reverse();
 for (let fruta of frutas) {
     console.log(fruta);
 }
+
+// let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"];
+
+console.log("=============================================");
+//Removendo itens do Array com indexOf e Splice
+frutas.splice(2, 1);
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("=============================================");
+//Removendo itens do Array com indexOf e Splice
+let indicePesquisa = frutas.indexOf("banana");
+console.log("INDICE ENCONTRADO : ", indicePesquisa);
+frutas.splice(indicePesquisa, 1);
+for (let fruta of frutas) {
+    console.log(fruta);
+}
