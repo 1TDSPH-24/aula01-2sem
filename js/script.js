dc30acd63468d48b6108b0419012d31506e8a1c2
felipepizzinato <felipepizzinato9@gmail.com>
2024-08-05 11:25:35 -0300

    lendo array com FOR tradicional

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//declarando um array
+
+let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"];
+
+//Declarando o conteúdo do ARRAY
+console.log(frutas);
+console.log(frutas[4]);
+console.table(frutas);
+
+//Realizando a leitura de um ARRAY com o FOR TRADICIONAL
+for (let x = 0; x < frutas.length; x++) {
+    console.log(frutas[x]);
+}
