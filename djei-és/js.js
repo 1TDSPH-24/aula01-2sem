06d6580cb2cd2a2e26c9ed20078574228f5f8f05
ovitortadeu <souavitor6@gmail.com>
2024-08-05 11:33:02 -0300

    farofa e for in

--- djei-és/js.js
+++ djei-és/js.js
@@ -7,8 +7,15 @@ console.log(futas);
 console.log(futas[0]);
 console.table(futas);
 
-// realizando a leitada de um for tradicional
+console.log("satanas morra agora ")
 
-for (let x = 0; x < futas.length; x++) {
-    console.log(futas[x])
+// realizando a leitada de um for of 
+for (let futa of futas) {
+    console.log(futa);
+}
+
+console.log("satanas morra agora ")
+
+for (let indice in futas) {
+    console.log(futas[indice]);
 }
