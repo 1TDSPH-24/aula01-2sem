17615e68769895735d2d198ecc8a2be695f1a25f
Rafa130206 <rafabompadre@gmail.com>
2024-08-05 11:33:05 -0300

    For OF e For IN

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ console.table(frutas);
 for (let x = 0; x < frutas.length; x++) {
     console.log(frutas[x]);
 }
+
+console.log("========================================");
+
+
+//Realizando a leitura d eum array com o FOR OF
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+
+console.log("========================================");
+
+
+//Realizando a leitura de um array com FOR IN
+for (let indice in frutas) {
+    console.log(indice);
+}
