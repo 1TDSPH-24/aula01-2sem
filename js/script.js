bd5136cfe2d32a11738d0df46582772b51793f7f
gustavodscruz <gustavodiasdsc@gmail.com>
2024-08-05 12:09:01 -0300

    functions array

--- js/script.js
+++ js/script.js
@@ -4,4 +4,17 @@ let frutas = ['zimbro', 'kiwi', 'yamamomo', 'banana', 'wampe'];
 //Visualizando o conteúdo do array:
 
 console.log(frutas);
-console.table(frutas)
+// console.table(frutas);
+let frutasRemovidas = frutas.shift()
+frutas.sort()
+console.log(frutas)
+frutas.reverse()
+console.log(frutas)
+console.log(frutasRemovidas)
+console.log('==========================')
+let indicePesquisa = frutas.indexOf('banana');
+console.log('ÍNDICE ENCONTRADO: ', indicePesquisa)
+frutas.splice(indicePesquisa, 1);
+for (let fruta of frutas) {
+    console.log(fruta)
+}
