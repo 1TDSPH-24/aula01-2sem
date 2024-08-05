8b8e0c48d2ae0d610b1fa3f7f97b7357aabe8bff
gustavodscruz <gustavodiasdsc@gmail.com>
2024-08-05 11:17:51 -0300

    let frutas e table

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("Bem vindo novamente!");
+
+let frutas = ['zimbro', 'kiwi', 'yamamomo', 'banana', 'wampe'];
+//Visualizando o conteúdo do array:
+
+console.log(frutas);
+console.table(frutas)
