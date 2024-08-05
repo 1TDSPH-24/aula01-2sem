a03a087717224a0b71d53db8a8be4a1cd5fc0568
enricodelguerra <ricodelguerra@gmail.com>
2024-08-05 11:26:05 -0300

    Lendo array com looping for tradicional.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+//Declarando um array
+let frutas = ["zimbro", "kiwi", "yamamomo", "Wampe"];
+//Visualizando o conteúdo do ARRAY
+console.log(frutas);
+
+console.table(frutas);
+
+//Realizando a leitura de um array com o for tradicional
+for(let x = 0; x < frutas.length ; x++){
+    console.log(frutas[x]);
+}
+
