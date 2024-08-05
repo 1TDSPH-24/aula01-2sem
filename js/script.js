920cd1fc06fadb24281ad9f6cdfb2f1461f5b68c
gusandrini <gustavosandrini92@gmail.com>
2024-08-05 11:46:31 -0300

    inserindo novos dados com PUSH e UNSHIFT

--- js/script.js
+++ js/script.js
@@ -25,3 +25,21 @@ console.log("============================================================");
 for (let indice in frutas) {
     console.log(frutas[indice]);
 }
+
+console.log("============================================================");
+
+//realizando a leitura de um array com um FOR EACH
+frutas.forEach(function (fruta, indice) {
+    console.log("COM FOR-EACH", fruta, indice);
+
+});
+
+console.log("============================================================");
+
+
+//inserindo novos dados com PUSH e UNSHIFT
+frutas.unshift("melancia");
+frutas.push("melão");
+for (let fruta of frutas) {
+    console.log(fruta);
+}
