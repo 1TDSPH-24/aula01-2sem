80b8a7e06851a44cdc71a848601c7b33bb1230fe
JuliaAngelozi <jumangelozi@gmail.com>
2024-08-05 12:10:27 -0300

    indexOf e splice

--- js/script.js
+++ js/script.js
@@ -23,3 +23,51 @@ for (let fruta of frutas) {
 for (let indice in frutas) {
     console.log(frutas[indice]);
 }
+
+//for each
+frutas.forEach(function (fruta, indice, frutasArray) {
+    console.log("COM FOR-EACH", fruta, frutasArray[indice]);
+});
+
+//Inserindo novos dados com PUSH(FIM) e UNSHIFT(INÍCIO)
+frutas.unshift("melancia");
+frutas.push("melão");
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+
+//Removendo dados com POP(FIM) e SHIFT(INÍCIO)
+frutas.shift();
+frutas.pop();
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+//Alterando a ordem dos dados com SORT(ordem alfábetica) e REVERSE(alterando a ordem atual).
+//frutas.sort();
+//for (let fruta of frutas) {
+//    console.log(fruta);
+//}
+
+
+//frutas.reverse();
+//for (let fruta of frutas) {
+//    console.log(fruta);
+//}
+
+//let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"];
+
+//Removendo itens do Array com indexOf e Splice
+frutas.splice(2, 1);
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+//Removendo itens do Array com indexOf e Splice
+let indicePesquisa = frutas.indexOf("banana");
+console.log("INDICE ENCONTRADO : ", indicePesquisa);
+frutas.splice(indicePesquisa, 1);
+for (let fruta of frutas) {
+    console.log(fruta);
+}
