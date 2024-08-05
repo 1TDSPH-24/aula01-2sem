1568bd34b03e013b737935b427fa0e2392ca1e59
ovitortadeu <souavitor6@gmail.com>
2024-08-05 11:25:15 -0300

    lendo array com loop for tradicional

---
+++ djei-és/js.js
@@ -0,0 +1,14 @@
+
+//declaracao de arrei
+
+let futas = ["zatanas", "kiwi", "yamamomo", "banana", "wampe"];
+// vizualizando o conteudo do array
+console.log(futas);
+console.log(futas[0]);
+console.table(futas);
+
+// realizando a leitada de um for tradicional
+
+for (let x = 0; x < futas.length; x++) {
+    console.log(futas[x])
+}
