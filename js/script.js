a5511218f3f701ccf96d10f44fd6b6a472abdb78
felipesora <felipeusora@gmail.com>
2024-08-05 11:32:59 -0300

    For OF e For IN

--- js/script.js
+++ js/script.js
@@ -6,7 +6,23 @@ console.log(frutas);
 console.log(frutas[4]);
 console.table(frutas);
 
+console.log("=====================================================================================");
+
 // Realizando a leitura de um ARRAY com o FOR tradicional
 for(let x = 0; x < frutas.length; x++) {
     console.log(frutas[x]);
+}
+
+console.log("=====================================================================================");
+
+// Realizando a leitura de um ARRAY com o FOR OF
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("=====================================================================================");
+
+// Realizando a leitura de um ARRAY com o FOR IN
+for (let indice in frutas) {
+    console.log(frutas[indice]);
 }
