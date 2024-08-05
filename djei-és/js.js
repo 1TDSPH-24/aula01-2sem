e46584fa9502e74a5942170eedb74ff85dd02aa8
ovitortadeu <souavitor6@gmail.com>
2024-08-05 11:51:44 -0300

    pushar, unshitft, pop, shift, sort e revertido

--- djei-és/js.js
+++ djei-és/js.js
@@ -19,3 +19,38 @@ console.log("satanas morra agora ")
 for (let indice in futas) {
     console.log(futas[indice]);
 }
+console.log("satanas morra agora ")
+
+for (let indice in futas) {
+    console.log(futas[indice]);
+}
+
+futas.forEach(function (futa, indice, futasarray) {
+    console.log("com foriti", futa, futasarray[indice])
+});
+
+//inseridno novos dados com push e unshigigigigi
+
+futas.unshift("melancia");
+futas.push("melãp");
+for (let futa of futas) {
+    console.log(futa);
+}
+
+
+// rempvemdp dadpsa com pop(final) e shift(iniquio)
+futas.shift()
+futas.pop()
+for (let futa of futas) {
+    console.log(futa);
+}
+
+// alterando ordem dos ngcs com sort(alfabetic) e reverse(ao contrario)
+futas.sort()
+for (let futa of futas) {
+    console.log(futa);
+}
+futas.reverse()
+for (let futa of futas) {
+    console.log(futa);
+}
