ba987f51a062f11d8ce6e3b8a06a6b1f99371b29
Rafa130206 <rafabompadre@gmail.com>
2024-08-05 11:51:59 -0300

    PUSH, UNSHIFT, POP, SHIFT, SORT e REVERSE.

--- js/script.js
+++ js/script.js
@@ -27,3 +27,41 @@ console.log("========================================");
 for (let indice in frutas) {
     console.log(indice);
 }
+
+
+console.log("========================================");
+//Realiznado leitura de um array com o FOR EACH
+frutas.forEach(function (fruta, indice, frutasArray) {
+    console.log("COM FOR-EACH", fruta, frutasArray[indice]);
+});
+
+console.log("========================================");
+//Inserindo novos dados com PUSH e UNSHIFT
+frutas.unshift("melancia")
+frutas.push("melão")
+
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("========================================");
+//Removendo novos dados com POP(FIM) e SHIFT(INÍCIO)
+frutas.shift()
+frutas.pop()
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+
+console.log("========================================");
+//Alterando a ordem dos dados com SORT(ordem alfabética) e REVERSE(alterando a ordem atual).
+frutas.sort()
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("========================================");
+frutas.reverse()
+for (let fruta of frutas) {
+    console.log(fruta);
+}
