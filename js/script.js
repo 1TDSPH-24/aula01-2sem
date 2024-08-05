3c363a55e1a9cef74594fbdfe796dbc5f5b786b9
GustavoAmorim12 <guamorim01@gmail.com>
2024-08-05 11:25:34 -0300

    lendo array com loop for tradicional

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"];
+
+console.log(frutas);
+console.log(frutas[4]);
+console.table(frutas);
+
+for (let x = 0; x < frutas.length; x++) {
+    console.log(frutas[x]);
+}
