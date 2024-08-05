3966eaf6f25de0a7ed87c96665de7ad296eb3a57
gusandrini <gustavosandrini92@gmail.com>
2024-08-05 11:18:33 -0300

    leitura de array com FOR

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 //declarando um array
 let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"];
+
+//visualizando o conteúdo do array
+console.log(frutas);
+console.log(frutas[4]);
+console.table(frutas);
+
+//realizando a leitura de um array com o for tradicional 
+for (let x = 0; )
