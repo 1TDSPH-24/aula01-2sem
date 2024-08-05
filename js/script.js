be96d0adc015f6e3135ed46f1b69c5da5b303de8
p3terhenry <pedro.antonieti@outlook.com>
2024-08-05 11:25:59 -0300

    Lendo array com estrutura de reptição for tradicional.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 //Declarando um Array
 let frutas = ["Zimbro", "Kiwi", "Yamamomo", "Banana", "Wampe"];
+
+console.log(frutas);
+console.log(frutas[4]);
+console.table(frutas);
+
+for(let x = 0; x < frutas.length; x++){
+    console.log(frutas[x]);
+}
