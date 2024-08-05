3b12057fab324ef2b5a3b28943f41769c782b489
lecristina <cristinale33@hotmail.com>
2024-08-05 12:11:50 -0300

    Removendo com indexOF e splice

--- js/scrpt.js
+++ js/scrpt.js
@@ -59,14 +59,32 @@ for (let fruta of frutas) {
 console.log("================================================");
 
 //Alterando a ordem dos dados com SORT(ordem alfabética) e REVERSE(alterando a ordem atual).
-frutas.sort();
-for (let fruta of frutas) {
-    console.log(fruta);
+// frutas.sort();
+// for (let fruta of frutas) {
+//     console.log(fruta);
 
-}
+// }
+// console.log("================================================");
+// frutas.reverse();
+// for (let fruta of frutas) {
+//     console.log(fruta);
+
+// }
+
+//let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"]
+
 console.log("================================================");
-frutas.reverse();
+//Removendo itens do Array com indexOF e Splice
+frutas.splice(2, 1);
 for (let fruta of frutas) {
     console.log(fruta);
+}
 
+console.log("=============================================");
+//Removendo itens do Array com indexOf e Splice
+let indicePesquisa = frutas.indexOf("banana");
+console.log("INDICE ENCONTRADO : ", indicePesquisa);
+frutas.splice(indicePesquisa, 3);
+for (let fruta of frutas) {
+    console.log(fruta);
 }
