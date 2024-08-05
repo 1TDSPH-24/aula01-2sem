fc98ddb6dcb8258b57b600dd3fe66ad16b42017a
vitoraraujo-144 <vitorvvaa@hotmail.com>
2024-08-05 11:52:13 -0300

    PUSH, UNSHIFT, POP, SHIFT,SORT e REVERSE.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,59 @@
+
 //Declarando um array
-let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"]
-//Visualisando o conteúdo do array
+let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"];
+//Visualizando o conteúdo do ARRAY:
 console.log(frutas);
-console.log(frutas[0]);
+console.log(frutas[4]);
 console.table(frutas);
 
-//Realizando a leitura de array com o FOR TRADICIONAL
+//Realizando a leitura de um array com o FOR TRADICIONAL
 for (let x = 0; x < frutas.length; x++) {
     console.log(frutas[x]);
 }
 
-console.log("================================================");
-
-
+console.log("=============================================");
 //Realizando a leitura de um array com o FOR OF
 for (let fruta of frutas) {
     console.log(fruta);
-
 }
+
+console.log("=============================================");
 
 //Realizando a leitura de um array com o FOR IN
 for (let indice in frutas) {
-    console.log(frutas[indice]);
+    console.log(indice);
+}
 
+console.log("=============================================");
+//Realizando a leitura de um array com o FOR IN
+frutas.forEach(function (fruta, indice, frutasArray) {
+    console.log("COM FOR-EACH", fruta, frutasArray[indice]);
+});
+
+
+console.log("=============================================");
+//Inserindo novos dados com PUSH(FIM) e UNSHIFT(INÍCIO)
+frutas.unshift("melancia");
+frutas.push("melão");
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("=============================================");
+//Removendo dados com POP(FIM) e SHIFT(INÍCIO)
+frutas.shift();
+frutas.pop();
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("=============================================");
+//Alterando a ordem dos dados com SORT(ordem alfábetica) e REVERSE(alterando a ordem atual).
+frutas.sort();
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+frutas.reverse();
+for (let fruta of frutas) {
+    console.log(fruta);
 }
