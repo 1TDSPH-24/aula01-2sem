cdaf775e4942adf3cc480a0ea10593d78db0610b
dudabrigidio <dudabrigidio@gmail.com>
2024-08-05 11:52:51 -0300

    unshift, push, shift pop, sort, reverse

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Declarando um array
-let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampeem"];
+let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampi"];
 
 //|Visualizando o conteúdo do ARRAY
 console.log(frutas);
@@ -19,4 +19,35 @@ for (let fruta of frutas) {
 //Realizando a leitura de um array com o FOR IN (retorna o indice em indice)
 for (let indice in frutas) {
     console.log(frutas[indice]);
+}
+
+//Realizando a leitura de um array com o FOR EACH
+frutas.forEach(function (fruta, indice, frutasArray) {
+    console.log("COM FOR-EACH", fruta, indice, frutasArray[indice]);
+});
+
+//Inserindo novos dados com UNSHIFT(INÍCIO) e PUSH(FIM) 
+frutas.unshift("melancia");
+frutas.push("melão");
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+//Removendo dados com SHIFT(INÍCIO) e POP(FIM)
+frutas.shift();
+frutas.pop();
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("=============================================");
+//Alterando a ordem dos dados com SORT(ordem alfábetica) e REVERSE(alterando a ordem atual).
+frutas.sort();
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+console.log("=============================================");
+frutas.reverse();
+for (let fruta of frutas) {
+    console.log(fruta);
 }
