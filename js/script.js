64124eb39d99dc63e8645f4e3275ad0fcd242475
p3terhenry <pedro.antonieti@outlook.com>
2024-08-05 11:33:04 -0300

    For of e For in.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 //Declarando um Array
 let frutas = ["Zimbro", "Kiwi", "Yamamomo", "Banana", "Wampe"];
 
-console.log(frutas);
-console.log(frutas[4]);
-console.table(frutas);
+//console.log(frutas);
+//console.log(frutas[4]);
+//console.table(frutas);
 
 for(let x = 0; x < frutas.length; x++){
     console.log(frutas[x]);
+}
+
+console.log("==========================================================");
+
+//Realizando a leitura de um array com o FOR OF. Pega uma item do frutas e coloca em fruta.
+for (let fruta of frutas){
+    console.log(fruta);
+}
+
+console.log("==========================================================");
+
+//Realizando a leitura de um array com FOR IN. Pega o valor do indice da fruta de frutas.
+for (let fruta in frutas) {
+    console.log(frutas[fruta]);
 }
