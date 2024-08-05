828f078710063146cda7a640fafb16e44d0d6936
felipepizzinato <felipepizzinato9@gmail.com>
2024-08-05 12:03:24 -0300

    aplicando métodos

--- js/script.js
+++ js/script.js
@@ -26,3 +26,47 @@ console.log("=============================================");
 for (let indice in frutas) {
     console.log(indice);
 }
+
+console.log("=============================================");
+
+//Realizando a leitura de um array com o FOR EACH
+frutas.forEach(function (frutas) {
+    console.log(frutas)
+});
+
+console.log("=============================================");
+//Inserindo novos dados com PUSH(FIM) e UNSHIFT(INÍCIO)
+frutas.unshift("melancia");
+frutas.push("melão");
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("=============================================");
+//Removendo dados com POP(FIM) e SHIFT(INÍCIO)
+frutas.shift();
+frutas.pop();
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log("=============================================");
+//Alterando a ordem dos dados com SORT(ordem alfábetica) e REVERSE(alterando a ordem atual).
+frutas.sort();
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+console.log("=============================================");
+frutas.reverse();
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+//let frutas = ["zimbro", "kiwi", "yamamomo", "banana", "wampe"];
+
+console.log("=============================================");
+//Removendo itens do Array com indexOf e Splice
+frutas.splice(2, 1);
+for (let fruta of frutas) {
+    console.log(fruta);
+}
